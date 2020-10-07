c2f5a5968b2bb5d153c63c2bc65096a644b300ce
RS Edge <sdagli@riskspan.com>
2020-10-06 22:52:25 -0400

    Add files via upload
    update return value in predict function

--- regressionex.py
+++ regressionex.py
@@ -16,13 +16,12 @@ def predict(X):
         rowsum = 0
         for cell in row:
             if (isinstance(cell,int) or isinstance(cell,float)):
-                print(cell)
                 rowsum = rowsum + cell*random.uniform(0,1)
             else:
-                print("string:",cell)
         rowsum = rowsum + random.uniform(0,1)
         Y.itemset((rownum,0),rowsum)
         rownum = rownum + 1
-    return Y
+    return pd.DataFrame(Y, columns=['pred_val'])
+
 
 
